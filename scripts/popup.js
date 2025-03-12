f57998b26a0cd832a26fb9abb7f2508f64f8c967
gdoyle87 <gary.doyle@gmail.com>
2025-03-12 16:38:17 -0700

    updated files missed on last commit

---
+++ scripts/popup.js
@@ -0,0 +1,10 @@
+$popup = $("#pop-up");
+$closeButton = $("#close-pop-up");
+$letsPlay = $("#lets_play");
+
+$closeButton.click(() => {
+    $popup.attr("hidden", true);
+})
+$letsPlay.click(() => {
+    $popup.attr("hidden", true);
+})
